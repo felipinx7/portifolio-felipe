c9420809981a59443e102b9741fdaa12fcc082aa
Felipe Dev <flimalimafelipek@gmail.com>
2025-12-31 18:33:31 -0300

    feat: photo of my projects added

---
+++ src/assets/images/index.ts
@@ -0,0 +1,11 @@
+import PhotoSystemAssisteciaSocial from "./photo-system-asssistencia-social.png";
+import PhotoSystemMassapeFascinante from "./photo-system-massape-fascinante.png";
+import PhotoSystemTopFitness from "./photo-system-topfitnes.png";
+import PhotoSystemHoobank from "./photo-system-hoobank.png";
+
+export {
+  PhotoSystemAssisteciaSocial,
+  PhotoSystemMassapeFascinante,
+  PhotoSystemTopFitness,
+  PhotoSystemHoobank,
+};
