3948871a4d0a0a1b93f917d627c151a8b9a377fa
Felipe Dev <flimalimafelipek@gmail.com>
2026-01-02 13:57:08 -0300

    feat: new icons utils in portifolio

---
+++ src/assets/icons/icon-bars-open.tsx
@@ -0,0 +1,20 @@
+import type { InterfaceClassIcon } from "../../interfaces/interface-class-icon";
+
+export function IconBarsOpen({ className }: InterfaceClassIcon) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+      />
+    </svg>
+  );
+}
