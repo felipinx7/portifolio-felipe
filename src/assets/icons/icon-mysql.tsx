8ca20af3ff2508348582889ccf8cd0df93025994
Felipe Dev <flimalimafelipek@gmail.com>
2026-05-01 10:48:37 -0300

    feat: new icons added in project

---
+++ src/assets/icons/icon-mysql.tsx
@@ -0,0 +1,29 @@
+import type { InterfaceClassIcon } from "../../interfaces/interface-class-icon";
+
+export function IconMySql({ className }: InterfaceClassIcon) {
+  return (
+    <svg
+      width="27"
+      height="26"
+      viewBox="0 0 27 26"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10.11 7.77734H10.1225"
+        stroke="white"
+        stroke-width="2.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className={className}
+      />
+      <path
+        d="M13.7463 23.4043C11.9625 22.1218 9.25879 18.5868 8.74629 15.9043C8.13879 16.8668 6.87004 18.4043 6.24629 18.4043C4.37254 17.2943 5.52879 13.438 6.24629 10.9043C4.25129 9.11302 3.16129 7.83177 3.12129 5.90427C-1.06621 1.60427 2.56629 -0.683235 6.24629 2.15427H7.49629C18.0988 2.77927 15.5225 12.2418 18.7463 16.5293C21.615 17.1818 23.3275 19.3468 24.9963 20.9043C22.3888 20.6543 21.5163 20.4743 20.6213 20.9043C21.2188 22.9543 23.275 23.6543 24.9963 24.6543"
+        stroke="white"
+        stroke-width="2.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
