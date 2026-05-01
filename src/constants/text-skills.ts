1821cd0c797af48fb74ee22d0d68aefe905ccfb4
Felipe Dev <flimalimafelipek@gmail.com>
2026-05-01 10:50:00 -0300

    fix: text update with news information

--- src/constants/text-skills.ts
+++ src/constants/text-skills.ts
@@ -1,20 +1,31 @@
+import { IconAWS } from "../assets/icons/icon-aws";
 import { IconCss } from "../assets/icons/icon-css";
+import { IconDocker } from "../assets/icons/icon-docker";
 import { IconEslint } from "../assets/icons/icon-eslint";
+import { IconFastify } from "../assets/icons/icon-fastify";
 import { IconFigma } from "../assets/icons/icon-figma";
 import { IconGithub } from "../assets/icons/icon-github";
 import { IconeHtml } from "../assets/icons/icon-html";
 import { IconJavaScript } from "../assets/icons/icon-javascript";
+import { IconMySql } from "../assets/icons/icon-mysql";
+import { IconNestJS } from "../assets/icons/icon-nestjs";
 import { IconNextJS } from "../assets/icons/icon-nextJS";
+import { IconNodeJS } from "../assets/icons/icon-nodejs";
+import { IconPhotoShop } from "../assets/icons/icon-photoshop";
+import { IconPostgress } from "../assets/icons/icon-postgresql";
 import { IconPrettier } from "../assets/icons/icon-prettier";
+import { IconPrisma } from "../assets/icons/icon-prisma";
 import { IconReact } from "../assets/icons/icon-react";
+import { IconSass } from "../assets/icons/icon-sass";
 import { IconStyledComponents } from "../assets/icons/icon-styled-components";
 import { IconTailwind } from "../assets/icons/icon-tailwind";
 import { IconTypescript } from "../assets/icons/icon-typescript";
 import { IconVite } from "../assets/icons/icon-vite";
+import { IconWordpress } from "../assets/icons/icon-wordpress";
 import { IconGit } from "../assets/icons/icon.git";
 import type { InterfaceSkills } from "../interfaces/interface-skills";
 
-export const textSkills: InterfaceSkills[] = [
+export const textSkillsFrontend: InterfaceSkills[] = [
   {
     icon: IconNextJS,
     nameStack: "NextJS",
@@ -52,6 +63,48 @@ export const textSkills: InterfaceSkills[] = [
     nameStack: "Tailwind",
   },
   {
+    icon: IconStyledComponents,
+    nameStack: "Styled C.",
+  },
+  {
+    icon: IconSass,
+    nameStack: "SASS",
+  },
+];
+
+export const textSkillsBackend: InterfaceSkills[] = [
+  {
+    icon: IconPrisma,
+    nameStack: "Prisma",
+  },
+  {
+    icon: IconPostgress,
+    nameStack: "Postgress",
+  },
+  {
+    icon: IconNodeJS,
+    nameStack: "NodeJS",
+  },
+  {
+    icon: IconNestJS,
+    nameStack: "NetJS",
+  },
+  {
+    icon: IconMySql,
+    nameStack: "MySQL",
+  },
+  {
+    icon: IconFastify,
+    nameStack: "Fastify",
+  },
+  {
+    icon: IconDocker,
+    nameStack: "Docker"
+  }
+];
+
+export const textSkillsTools: InterfaceSkills[] = [
+  {
     icon: IconGit,
     nameStack: "Git",
   },
@@ -60,15 +113,23 @@ export const textSkills: InterfaceSkills[] = [
     nameStack: "Github",
   },
   {
-    icon: IconStyledComponents,
-    nameStack: "Styled C.",
-  },
-  {
     icon: IconEslint,
     nameStack: "ESlint",
   },
   {
     icon: IconPrettier,
     nameStack: "Prettier",
+  },
+  {
+    icon: IconWordpress,
+    nameStack: "Wordpress",
+  },
+  {
+    icon: IconPhotoShop,
+    nameStack: "Photoshop",
+  },
+  {
+    icon: IconAWS,
+    nameStack: "AWS",
   },
 ];
